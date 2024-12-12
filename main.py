1f5ab0a43a3cae44a4ce3183ae2954a257135e7a
Prakaidaw <121006933+PKD-J@users.noreply.github.com>
2024-12-12 13:41:50 +0700

    version1.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ ACCESS_TOKEN = "example"
 CHANNEL_SECRET = "example"
 
 # ข้อมูล Gemini api key
-GEMINI_API_KEY = "example"
+GEMINI_API_KEY = "AIzaSyBcjYfsh-m8p-91uBLwbcFS7_Owl48G07g"
 
 # การเชื่อมต่อ และตั้งค่าข้อมูลเพื่อเรียกใช้งาน LINE Messaging API
 configuration = Configuration(access_token=ACCESS_TOKEN)
